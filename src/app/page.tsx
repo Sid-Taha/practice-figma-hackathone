65edc95917a18321a6cbb283592d3895edadf2c2
Sid-Taha <twahasiddqui11@gmail.com>
2024-12-05 19:09:09 +0500

    up2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import FlashSale from "@/components/flashSale";
 import Hero from "@/components/hero";
 
 export default function Home() {
+ //-----------------------------------tahaaaaaaaaaaaaaaaaaaaa
   return (
     <div className="px-[135px] overflow-hidden">
       <Hero/>
